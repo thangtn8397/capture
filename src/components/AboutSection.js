ec681dffabc1e37ed98e9f8a1d58b9ed303ed487
thangtn8397 <thangtn8397@gmail.com>
2021-03-21 19:26:50 +0700

    About Section

---
+++ src/components/AboutSection.js
@@ -0,0 +1,30 @@
+import React from "react";
+import home1 from "../img/home1.png";
+
+const AboutSection = () => {
+  return (
+    <div>
+      <div className="description">
+        <div className="title">
+          <div className="hide">
+            <h2>We work to make</h2>
+          </div>
+          <div className="hide">
+            <h2>
+              your <span>dreams</span> come
+            </h2>
+          </div>
+          <div className="hide">
+            <h2></h2>
+            true.
+          </div>
+        </div>
+      </div>
+      <div className="image">
+        <img src={home1} alt="" srcset="" />
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
